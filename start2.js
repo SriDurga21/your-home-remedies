ecc512d3104357b3c1ef4c80a7d75f8cd8013d5c
SriDurga21 <sridurgaraavi@gmail.com>
2026-04-16 22:28:56 +0530

    Add files via upload

---
+++ start2.js
@@ -0,0 +1,89 @@
+function cough1(){
+  window.location.href="cough1.html";
+}
+function hairfall1(){
+  window.location.href="hairfall1.html";
+}
+function headacha1(){
+  window.location.href="headacha1.html";
+}
+function irondeficiency1(){
+  window.location.href="irondeficiency1.html";
+}
+function kidneystones1(){
+  window.location.href="kidneystones1.html";
+}
+function legspain1(){
+  window.location.href="legspain1.html";
+}
+function mouthulcer1(){
+  window.location.href="mouthulcer1.html";
+}
+function stomachacha1(){
+  window.location.href="stomachacha1.html";
+}
+function sugarcontrol1(){
+  window.location.href="sugarcontrol1.html";
+}
+function swallowing1(){
+  window.location.href="swallowing1.html";
+}
+function weigthloss1(){
+  window.location.href="weigthloss1.html";
+}
+function gastric1(){
+  window.location.href="gastric1.html";
+}
+function fever1(){
+  window.location.href="fever1.html";
+}
+function asthma1(){
+  window.location.href="asthma1.html";
+}
+function staranise1(){
+  window.location.href="staranise1.html";
+}
+function garlic1(){
+  window.location.href="garlic1.html";
+}
+function ginger1(){
+  window.location.href="ginger1.html";
+}
+function cardamom1(){
+  window.location.href="cardamom1.html";
+}
+function cinnamon1(){
+  window.location.href="cinnamon1.html";
+}
+function coriander1(){
+  window.location.href="coriander1.html";
+}
+function blackpepper1(){
+  window.location.href="blackpepper1.html";
+}
+function cumin1(){
+  window.location.href="cumin1.html";
+}
+function fenugreek1(){
+  window.location.href="fenugreek1.html";
+}
+function cloves1(){
+  window.location.href="cloves1.html";
+}
+function fennel1(){
+  window.location.href="fennel1.html";
+}
+function turmeric1(){
+  window.location.href="turmeric1.html";
+}
+function onion1(){
+  window.location.href="onion1.html";
+}
+function carrot1(){
+  window.location.href="carrot1.html";
+}
+function cucumber1(){
+  window.location.href="cucumber1.html";
+}
+  
+
